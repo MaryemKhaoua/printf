bc7abe1e9063e28b780951e997b2b5be0b848e68
Maryem khaoua <maryem.khaoua@gmail.com>
2023-04-25 05:33:06 -0700

    another test

--- _printf.c
+++ _printf.c
@@ -1,67 +1,105 @@
 #include "main.h"
+#include <stdarg.h>
+
 /**
- * _printf - Entry Function
- * @format: char
- * Return: the number of characters printed
+ * print_str - prints a string
+ * @arg: va_list containing the string to print
+ *
+ * Return: number of characters printed
+ */
+int print_str(va_list arg)
+{
+        char *str = va_arg(arg, char *);
+        int i = 0;
+
+        if (str == NULL)
+                str = "(null)";
+        while (str[i])
+        {
+                _putchar(str[i]);
+                i++;
+        }
+        return (i);
+}
+
+/**
+ * print_char - prints a character
+ * @arg: va_list containing the character to print
+ *
+ * Return: number of characters printed
+ */
+int print_char(va_list arg)
+{
+        _putchar(va_arg(arg, int));
+        return (1);
+}
+
+/**
+ * print_percent - prints a percent sign
+ * @arg: unused
+ *
+ * Return: number of characters printed
+ */
+int print_percent(va_list arg __attribute__((unused)))
+{
+        _putchar('%');
+        return (1);
+}
+
+/**
+ * _printf - prints a formatted string to the console
+ * @format: the format string
+ * @...: optional arguments
+ *
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list mym;
-	int ncp = 0;
+        va_list arg;
+        int i = 0, count = 0;
+        int (*print_fn)(va_list);
 
-	va_start(mym, format);
-	for (; *format != '\0'; format++)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0') /* handle an invalid format specifier */
-			{
-				va_end(mym);
-				return (-1);
-			}
-			else if (*format == '%') /* handle escaped % character */
-			{
-				write(STDOUT_FILENO, "%", 1);
-				ncp++;
-			}
-			else if (*format == 'c') /* handle char format specifier */
-			{
-				char c = va_arg(mym, int);
+        va_start(arg, format);
 
-				write(STDOUT_FILENO, &c, 1);
-				ncp++;
-			}
-			else if (*format == 's') /* handle string format specifier */
-			{
-				char *str = va_arg(mym, char *);
+        while (format && format[i])
+        {
+                if (format[i] == '%')
+                {
+                        i++;
+                        print_fn = NULL;
+                        switch (format[i])
+                        {
+                                case 's':
+                                        print_fn = print_str;
+                                        break;
+                                case 'c':
+                                        print_fn = print_char;
+                                        break;
+                                case '%':
+                                        print_fn = print_percent;
+                                        break;
+                                default:
+                                        _putchar('%');
+                                        if (format[i])
+                                                _putchar(format[i]);
+                                        else
+                                                i--;
+                                        count += 2;
+                                        break;
+                        }
 
-				if (str == NULL) /* handle NULL string argument */
-				{
-					write(STDOUT_FILENO, "(null)", 6);
-					ncp += 6;
-				}
-				else
-				{
-					while (*str != '\0')
-					{
-						write(STDOUT_FILENO, str, 1);
-						str++;
-						ncp++;
-					}
-				}
-			}
-			else /* handle an invalid format specifier */
-			{
-				write(STDOUT_FILENO, "%", 1);
-				write(STDOUT_FILENO, format, 1);
-				ncp += 2; }
-		}
-		else /* handle regular character */
-		{
-			write(STDOUT_FILENO, format, 1);
-			ncp++;
-		}
-	}
-	va_end(mym);
-	return (ncp); }
+                        if (print_fn != NULL)
+                                count += print_fn(arg);
+                }
+                else
+                {
+                        _putchar(format[i]);
+                        count++;
+                }
+                i++;
+        }
+
+        va_end(arg);
+        return (count);
+}
+
