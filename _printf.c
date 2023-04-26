ff74f5b5421813e8d5904815d827115f8e2fdd01
Maryem khaoua <maryem.khaoua@gmail.com>
2023-04-25 19:28:05 -0700

    last one

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(mym, format);
-	for (; *format != '\0'; format++)
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
