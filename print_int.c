62c6134d11a8e518ab011ebd490f2f2678921b68
Niama El hrychy <elhrychy.niama1337@gmail.com>
2023-04-26 17:16:46 -0700

    ok

--- print_int.c
+++ print_int.c
@@ -1,46 +1,14 @@
 #include "main.h"
 /**
- * print_int - prints an integer to stdout
- * @n: the integer to print
- * Return: number of characters printed
+ * print_integer - a function that prints an integer similar to printf
+ * @n: the integer to be printed
+ * Return: the number of characters printed
  */
-int print_int(int n)
+int print_integer(int n)
 {
-	int ncp = 0;
-	char buffer[20]; /* enough to hold any 32-bit integer */
-	int is_negative = 0;
+	char buffer[20];
+	int len = snprintf(buffer, 20, "%d", n);
 
-	if (n == 0)
-	{
-		write(STDOUT_FILENO, "0", 1);
-		ncp++; }
-	else if (n == INT_MIN) /* handle the case for INT_MIN */
-	{
-		write(STDOUT_FILENO, "-2147483648", 11);
-		ncp += 11; }
-	else
-	{
-		int i = 0;
-
-		if (n < 0)
-		{
-			is_negative = 1;
-			n = -n;
-			ncp++;
-		}
-		while (n > 0)
-		{
-			buffer[i] = (n % 10) + '0';
-			n /= 10;
-			i++;
-		}
-		if (is_negative)
-		{
-			buffer[i] = '-';
-			i++; }
-		while (i > 0)
-		{
-			i--;
-			write(STDOUT_FILENO, &buffer[i], 1);
-			ncp++; } }
-	return (ncp); }
+	write(STDOUT_FILENO, buffer, len);
+	return (len);
+}
